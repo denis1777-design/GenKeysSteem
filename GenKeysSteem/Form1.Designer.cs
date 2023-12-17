9a4190ea8a8721a7030e9d4a2a4bb4e79228740f
Denis <denisg7@outlook.com>
2023-12-17 16:45:35 +0300

    Добавьте файлы проекта.

---
+++ GenKeysSteem/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace GenKeysSteem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numOfKey = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numOfKey)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.textBox1.Location = new System.Drawing.Point(30, 113);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(437, 200);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // numOfKey
+            // 
+            this.numOfKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numOfKey.Location = new System.Drawing.Point(30, 74);
+            this.numOfKey.Name = "numOfKey";
+            this.numOfKey.Size = new System.Drawing.Size(437, 20);
+            this.numOfKey.TabIndex = 1;
+            this.numOfKey.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(30, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(437, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "RUN Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Выберите количество ключей от 0 до 100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(238, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Список сгенерированных ключей для STEEM";
+            // 
+            // btn2
+            // 
+            this.btn2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btn2.Location = new System.Drawing.Point(30, 372);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(437, 23);
+            this.btn2.TabIndex = 5;
+            this.btn2.Text = "Генерировать пароли";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button2.Location = new System.Drawing.Point(30, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(437, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Генерировать PIN-code";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.button3.Location = new System.Drawing.Point(496, 331);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(292, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Генерировать Никнейм";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(516, 113);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(243, 200);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 17.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(777, 40);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Генератор паролей, ключей для стим, пин-кодов и никнеймов";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numOfKey);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numOfKey)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numOfKey;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ GenKeysSteem/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace GenKeysSteem
+{
+    public partial class Form1 : Form
+    {
+        private const string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        private const string characters2 =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZzyxwvutsrqponmlkjihgfedcba@!#$%^&<>{}[]*()_-=+`~0123456789";
+
+        private const string numb = "0123456789";
+
+        private const string glasnie = "aeuoyi";
+
+        private const string soglasnie = "qwrtpsdfghjklzxcvbnm";
+
+        // это массив из char, т.е. эл.с индексом 0 будет соответствовать буква А
+        Random random = new Random(); //создаем экземпляр класса Random
+
+        string GenerateKeyPart()
+        {
+
+            string keyPart = string.Empty;
+            for (int i = 0; i < 5; i++)
+            {
+                keyPart += characters[random.Next(1, characters.Length)];
+            }
+
+            return keyPart;
+        }
+
+        string GeneratePass()
+        {
+
+            string keyPart = string.Empty;
+            for (int i = 0; i < 9; i++)
+            {
+                keyPart += characters2[random.Next(1, characters2.Length)];
+            }
+
+            return keyPart;
+        }
+
+        string GeneratePin()
+        {
+            string keyPart = string.Empty;
+            for (int i = 0; i < 4; i++)
+            {
+                keyPart += numb[random.Next(1, numb.Length)];
+            }
+
+            return keyPart;
+        }
+
+        string GenerateLogin()
+        {
+
+            string keyPart = string.Empty;
+            for (int i = 0; i < 3; i++)
+            {
+                keyPart += glasnie[random.Next(1, glasnie.Length)];
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+
+                keyPart += soglasnie[random.Next(1, soglasnie.Length)];
+            }
+
+            byte nameLen = 6;
+
+            return keyPart;
+
+
+        }
+
+        string GenerateLogin2()
+        {
+
+            string keyPart = string.Empty;
+            for (int i = 0; i < 3; i++)
+            {
+                keyPart += glasnie[random.Next(1, glasnie.Length)];
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+
+                keyPart += soglasnie[random.Next(1, soglasnie.Length)];
+            }
+
+
+            /*
+            byte nameLen = 6;
+            bool firstChar = true;
+
+            while (keyPart.Length < nameLen)
+            {
+                var charArray = keyPart.ToCharArray();
+                charArray = new char[6];
+                if (charArray ="") {
+                }
+            }
+            */
+            return keyPart;
+
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //textBox1.Text += GenerateKeyPart();  если написать эту строку без цикла то ключи будут генерироваться в одну строку
+
+            textBox1.Text = string.Empty;
+
+            for (int i = 0; i < numOfKey.Value; i++)
+            {
+                textBox1.Text += $"{GenerateKeyPart()}-{GenerateKeyPart()}-{GenerateKeyPart()}\r\n";
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+
+            for (int i = 0; i < numOfKey.Value; i++)
+            {
+                textBox1.Text += $"{GeneratePass()}\r\n";
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+
+            for (int i = 0; i < numOfKey.Value; i++)
+            {
+                textBox1.Text += $"{GeneratePin()}\r\n";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Text = string.Empty;
+
+            for (int i = 0; i < numOfKey.Value; i++)
+            {
+                textBox1.Text += $"{GenerateLogin()}\r\n";
+            }
+            for (int i = 0; i < numOfKey.Value; i++)
+            {
+                textBox2.Text += $"{GenerateLogin2()}\r\n";
+            }
+
+        }
+    }
+}
